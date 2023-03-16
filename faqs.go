0e7965c754c46b285ba7999dcd37b28fe6efc177
Marcel-NG <uduma.matrix@gmail.com>
2023-03-16 20:16:55 +0100

    Added the ability to read the json from the file system, as json and then added the edit file to work from the exact file contents

--- faqs.go
+++ faqs.go
@@ -3,11 +3,14 @@ package main
 // onyi.wait.oh
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+	"strconv"
 )
 
 var tmpl *template.Template
@@ -18,52 +21,60 @@ type Faq struct {
 	Answer   string
 }
 
+// This is the best data struct to use for the Faqs
+type FaqData struct {
+	Faqs []Faq `json:"faqs"`
+}
+
+type EditPage struct {
+	Title string
+	Faq   Faq
+}
+
 type AllPostPage struct {
 	Title string
 	Faqs  []Faq
 }
 
-var all_posts = []Faq{
-	{
-		ID:       1,
-		Question: "Question One",
-		Answer:   "eligendi explicabo ducimus cum inventore eos. blanditiis ullam distinctio qui",
-	},
-	{
-		ID:       2,
-		Question: "Question Two",
-		Answer:   "eligendi explicabo ducimus cum inventore eos. delectus repellendus perferendis tempora labore odio",
-	},
-	{
-		ID:       3,
-		Question: "Question Three",
-		Answer:   "eligendi explicabo ducimus cum inventore eos.",
-	},
-	{
-		ID:       4,
-		Question: "Question Four",
-		Answer:   "eligendi explicabo ducimus cum inventore eos. ea temporibus praesentium",
-	},
-}
-
 var tmpl_dir = "templates"
 
+func getFagsJSON() (*FaqData, error) {
+	var faq FaqData
+	body, err := os.ReadFile("./data/faqs.json")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(body, &faq); err != nil {
+		return nil, err
+	}
+	return &faq, nil
+}
+
 func allFaqs(w http.ResponseWriter, r *http.Request) {
+
+	v, e := getFagsJSON()
+	if e != nil {
+		fmt.Print(e)
+	}
 	all_post := AllPostPage{
 		Title: "Faqs Page",
-		Faqs:  all_posts,
+		Faqs:  v.Faqs,
 	}
 	tmpl = template.Must(template.ParseFiles(tmpl_dir + "/index.html"))
-
 	tmpl.Execute(w, all_post)
 }
 
 func faqAdmin(w http.ResponseWriter, r *http.Request) {
-	tmpl = template.Must(template.ParseFiles(tmpl_dir + "/admin.html"))
+	v, e := getFagsJSON()
+	if e != nil {
+		fmt.Print(e)
+	}
 	faqs := AllPostPage{
 		Title: "Admin Page",
-		Faqs:  all_posts,
+		Faqs:  v.Faqs,
 	}
+	tmpl = template.Must(template.ParseFiles(tmpl_dir + "/admin.html"))
 	tmpl.Execute(w, faqs)
 }
 
@@ -75,24 +86,33 @@ func faqEdit(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	// return m[2], nil //The title is the second expression
-	fmt.Println(m)
+	faqId, err := strconv.Atoi(m[2])
+
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	v, e := getFagsJSON()
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	var Faqs = v.Faqs
+	var ff Faq
+
+	for _, singleFaq := range Faqs {
+		if singleFaq.ID == faqId {
+			ff = singleFaq
+		}
+	}
 
 	tmpl = template.Must(template.ParseFiles(tmpl_dir + "/edit.html"))
-
-	tmpl.Execute(w, "Joy")
-}
-
-func faqCreate(w http.ResponseWriter, r *http.Request) {
-	tmpl_dir := "templates"
-	tmpl = template.Must(template.ParseFiles(tmpl_dir + "/create.html"))
-}
-
-func handlePageNotFound(w http.ResponseWriter, r *http.Request) {
-	tmpl_dir := "templates"
-	tmpl = template.Must(template.ParseFiles(tmpl_dir + "/404.html"))
-
-	tmpl.Execute(w, nil)
+	page := EditPage{
+		Title: "Edit Page",
+		Faq:   ff,
+	}
+	tmpl.Execute(w, page)
 }
 
 func main() {
@@ -105,9 +125,7 @@ func main() {
 
 	mux.HandleFunc("/faqs/all", allFaqs)
 	mux.HandleFunc("/faqs/admin", faqAdmin)
-	mux.HandleFunc("/faqs/create", faqCreate)
 	mux.HandleFunc("/faqs/edit/", faqEdit)
-	// mux.HandleFunc("/", handlePageNotFound)
 
 	log.Fatal(http.ListenAndServe(":9092", mux))
 }
